a5dfbaa7c65ee5ea6059b69530a6dc40d2f7595f
josph130 <yilmazdroid@gmail.com>
2025-05-28 23:33:47 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,13 +1,30 @@
-from datetime import datetime
+from datetime import date, timedelta
 import os
 
-# Dosya üretimi
-#file_name = f"output_{datetime.now().strftime('%Y%m%d')}.txt"
-file_name = f"file.txt"
-file_path = f"outputs/{file_name}"  # outputs klasörüne kaydedeceğiz
-os.makedirs("outputs", exist_ok=True)  # Klasörü oluştur (varsa hata verme)
+todayS = date.today()
+magasins = ["bc", "back-to-bc", "calvinandhobbes", "garfield",  "heathcliff",  "nancy", "nancy-classics", "peanuts", "peanuts-begins", "wizardofid", "wizard-of-id-classics"]
 
-with open(file_path, 'w') as f:
-    f.write(f"Bu dosya {datetime.now()} tarihinde GitHub Actions tarafından üretildi.")
+for magasin in magasins:
+    file_name = f"{magasin}.rss"
+    file_path = f"outputs/{file_name}"  # outputs klasörüne kaydedeceğiz
+    dateF = date.today() - timedelta(days=91)
 
-print(f"{file_name} üretildi.")
+    with open(file_path, 'w') as file:
+        file.write(f"<?xml version="1.0" encoding="utf-8"?>")
+        file.write(f"<rss version="2.0">")
+        file.write(f"<channel>")
+        file.write(f"<title>{magasin}</title>")
+        file.write(f"<link>http://www.gocomics.com/{magasin}</link>")
+        file.write(f"<description>Gocomics {magasin}</description>")
+        while dateF <= todayS:
+            file.write(f"<item>")
+            file.write(f"<title>{magasin} {dateF.strftime("%-d %B %Y")}</title>")
+            file.write(f"<link>http://www.gocomics.com/{magasin}/{dateF.strftime("%Y/%m/%d")}</link>")
+            file.write(f"<guid>http://www.gocomics.com/{magasin}/{dateF.strftime("%Y/%m/%d")}</guid>")
+            file.write(f"<pubDate>{dateF.strftime("%a, %-d %b %Y")} 13:00:00 GMT</pubDate>")
+            file.write(f"<description>{magasin} {dateF.strftime("%-d %B %Y")}</description>")
+            file.write(f"</item>")
+            dateF += timedelta(days=1)
+        file.write(f"</channel>")
+        file.write(f"</rss>")
+        file.close()
