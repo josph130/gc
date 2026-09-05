3d9702b85472d5b459ddbc71f98ce7b06cfbcf1c
josph130 <yilmazdroid@gmail.com>
2026-09-05 15:13:58 +0300

    Add comic RSS feed generation to script.py

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ import os
 
 todayS = date.today()
 magasins = ["bc", "back-to-bc", "calvinandhobbes", "garfield",  "heathcliff",  "nancy", "nancy-classics", "peanuts", "peanuts-begins", "wizardofid", "wizard-of-id-classics"]
+comics = ["popeye", "dennis-the-menace"]
 
 for magasin in magasins:
     file_name = f"{magasin}.rss"
@@ -23,6 +24,31 @@ for magasin in magasins:
             file.write(f"<guid>https://www.gocomics.com/{magasin}/{dateF.strftime("%Y/%m/%d")}</guid>\n")
             file.write(f"<pubDate>{dateF.strftime("%a, %-d %b %Y")} 13:00:00 GMT</pubDate>\n")
             file.write(f"<description>{magasin} {dateF.strftime("%-d %B %Y")}</description>\n")
+            file.write(f"</item>\n")
+            dateF += timedelta(days=1)
+        file.write(f"</channel>\n")
+        file.write(f"</rss>")
+        file.close()
+
+for comic in comics:
+    file_name = f"{comic}.rss"
+    file_path = f"outputs/{file_name}"  # outputs klasörüne kaydedeceğiz
+    dateF = date.today() - timedelta(days=91)
+
+    with open(file_path, 'w') as file:
+        file.write(f"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n")
+        file.write(f"<rss version=\"2.0\">\n")
+        file.write(f"<channel>\n")
+        file.write(f"<title>{comic}</title>\n")
+        file.write(f"<link>https://www.comicskingdom.com/{comic}</link>\n")
+        file.write(f"<description>Comics Kingdom {comic}</description>\n")
+        while dateF <= todayS:
+            file.write(f"<item>\n")
+            file.write(f"<title>{comic} {dateF.strftime("%-d %B %Y")}</title>\n")
+            file.write(f"<link>https://www.comicskingdom.com/{comic}/{dateF.strftime("%Y/%m/%d")}</link>\n")
+            file.write(f"<guid>https://www.comicskingdom.com/{comic}/{dateF.strftime("%Y/%m/%d")}</guid>\n")
+            file.write(f"<pubDate>{dateF.strftime("%a, %-d %b %Y")} 13:00:00 GMT</pubDate>\n")
+            file.write(f"<description>{comic} {dateF.strftime("%-d %B %Y")}</description>\n")
             file.write(f"</item>\n")
             dateF += timedelta(days=1)
         file.write(f"</channel>\n")
